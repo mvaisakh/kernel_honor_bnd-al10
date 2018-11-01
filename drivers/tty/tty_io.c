34db270b16dcd58ed929795ef1f2aacc8c2e5ebb
Dmitry Safonov <dima@arista.com>
2018-11-01 00:24:49 +0000

    tty: Simplify tty->count math in tty_reopen()
    commit cf62a1a13749db0d32b5cdd800ea91a4087319de upstream.
    
    As notted by Jiri, tty_ldisc_reinit() shouldn't rely on tty counter.
    Simplify math by increasing the counter after reinit success.
    
    Cc: Jiri Slaby <jslaby@suse.com>
    Link: lkml.kernel.org/r/<20180829022353.23568-2-dima@arista.com>
    Suggested-by: Jiri Slaby <jslaby@suse.com>
    Reviewed-by: Jiri Slaby <jslaby@suse.cz>
    Tested-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Dmitry Safonov <dima@arista.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -1492,9 +1492,16 @@ static int tty_reopen(struct tty_struct *tty)
 	if (tty->ldisc)
 		return 0;
 
-	retval = tty_ldisc_reinit(tty, tty->termios.c_line);
+	retval = tty_ldisc_lock(tty, 5 * HZ);
 	if (retval)
-		tty->count--;
+		return retval;
+
+	if (!tty->ldisc)
+		retval = tty_ldisc_reinit(tty, tty->termios.c_line);
+	tty_ldisc_unlock(tty);
+
+	if (retval == 0)
+		tty->count++;
 
 	return retval;
 }
