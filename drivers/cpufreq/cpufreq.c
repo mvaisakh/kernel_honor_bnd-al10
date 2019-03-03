591391e985c1db6612e696b299fee4a0fad61dc4
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-03 13:50:58 -0800

    cpufreq: Don't let userspace set the max freq
    Now that msm-thermal-simple is in use, it'll just override any userspace
    requests to change the max freq anyway. Disable writing to the max freq
    at all.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Vaishnav Murali <vaishnavmurali@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -752,6 +752,7 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
+									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
 	new_policy.min = policy->user_policy.min;			\
 	new_policy.max = policy->user_policy.max;			\
